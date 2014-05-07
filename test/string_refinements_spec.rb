c1fba13a2b4a753db531d92255260890c1adba93
Horst Mumpitz <denniskluge@me.com>
2014-05-07 20:25:19 +0200

    clean up

--- test/string_refinements_spec.rb
+++ test/string_refinements_spec.rb
@@ -1,10 +1,9 @@
 require "minitest/autorun"
 require "morse_code"
+
 using MorseCode::Refinements
 
 describe String do 
-  # eval("using MorseCode::Refinements")
-
   it "converts a string to its morse code equilavent" do 
     "hello world".to_morse_code.must_equal ".... . .-.. .-.. --- .-- --- .-. .-.. -.."
   end
